40bea924c5dc7711093d6ee34abc9f5f069c036d
shaochi <0419mmgc7@gmail.com>
2023-11-04 14:49:46 +0800

    update

--- hw3/src/app/api/joins/route.ts
+++ hw3/src/app/api/joins/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import { and, eq, sql } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
 import { db } from "@/db";
